622d5f42b8b62e10dba506658c3b44afdd0367fc
pushpitaafrin100-droid <pushpitaafrin000@gmail.com>
2026-07-30 13:02:30 +0600

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,13 @@
+public class Arithemetic {
+    public static void main(String[] args) {
+        int a = -5 + 8 * 6;              // Output: 43
+        int b = (55 + 9) % 9;             // Output: 1
+        int c = 20 + -3 * 5 / 8;          // Output: 19
+        int d = 5 + 15 / 3 * 2 - 8 % 3;   // Output: 13
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 3;
+        int result = a / b;
+        System.out.println(result);
+    }
+}
